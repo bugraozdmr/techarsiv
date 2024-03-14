b46ea24835df2f87db61d96c1ee115ee8a5f621a
bugra <@gmail.com>
2024-03-14 22:55:28 +0300

    fixes.

--- Repositories/Config/TagConfig.cs
+++ Repositories/Config/TagConfig.cs
@@ -9,10 +9,15 @@ public class TagConfig : IEntityTypeConfiguration<Tag>
     public void Configure(EntityTypeBuilder<Tag> builder)
     {
         builder.HasData(
-            new Tag() {TagId = 1,TagName = "Haber",Url = "haber"},
-            new Tag() {TagId = 2,TagName = "Makale",Url = "makale"},
-            new Tag() {TagId = 3,TagName = "Araba",Url = "araba"},
-            new Tag() {TagId = 4,TagName = "Dijital",Url = "dijital"}
+            new Tag() {TagId = 1,TagName = "Oyun haberleri",Url = "oyun-haber"},
+            new Tag() {TagId = 2,TagName = "Sinema haberleri",Url = "sinema-haber"},
+            new Tag() {TagId = 3,TagName = "Vasıtalar",Url = "vasitalar"},
+            new Tag() {TagId = 4,TagName = "Telefon",Url = "telefon"},
+            new Tag() {TagId = 5,TagName = "Teknoloji",Url = "teknoloji"},
+            new Tag() {TagId = 6,TagName = "Tavsiyeler",Url = "tavsiyeler"},
+            new Tag() {TagId = 7,TagName = "Sistem",Url = "sistem"},
+            new Tag() {TagId = 8,TagName = "haber",Url = "haber"}
+            
             );
     }
 }
