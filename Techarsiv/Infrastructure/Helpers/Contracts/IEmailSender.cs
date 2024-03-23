3b6fc34b2350225204a0f53813d9ff361e100581
bugra <@gmail.com>
2024-03-24 01:29:01 +0300

    Coomit anyway

--- Techarsiv/Infrastructure/Helpers/Contracts/IEmailSender.cs
+++ Techarsiv/Infrastructure/Helpers/Contracts/IEmailSender.cs
@@ -4,4 +4,5 @@ public interface IEmailSender
 {
     Task<int> SendEmailAsync(string email, string subject, string message);
     Task SendMultipleEmailsAsync(IEnumerable<string> emails, string subject, string message);
+    
 }
