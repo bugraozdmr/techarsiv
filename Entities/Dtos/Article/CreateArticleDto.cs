41582dc558ed62a212905434e24d04bf34cc560e
bugra <@gmail.com>
2024-03-23 12:32:17 +0300

    image fix

--- Entities/Dtos/Article/CreateArticleDto.cs
+++ Entities/Dtos/Article/CreateArticleDto.cs
@@ -6,7 +6,6 @@ public record CreateArticleDto
 {
     [Required(ErrorMessage = "Başlık alanı doldurulmalı")]
     [DataType(DataType.Text)]
-    [MaxLength(60,ErrorMessage = "Başlık 60 karakterden fazla olamaz")]
     [MinLength(10,ErrorMessage = "Başlık 10 karakterden uzun olmalı")]
     public string Title { get; init; }
     
